fefa9c46abd99638595ff520b105375af87d7f55
Hugo_A <hugoagnola@gmail.com>
2021-02-10 05:36:55 -0800

    Initial commit

---
+++ Assets/HitboxController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxController : MonoBehaviour
+{
+
+    public GameObject Target;
+    public Vector3 TargetPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetPos = Target.transform.position;
+        this.transform.position = TargetPos;
+    }
+}
---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public GameObject hitbox;
+    private int money;
+    private bool collision_state;
+    public Text MoneyText;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (collision_state == true) {
+            AddMoney();
+            collision_state = false;
+        }
+    }
+
+    
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.gameObject == hitbox)
+        {
+            Debug.Log("Collision Detected");
+            collision_state = true;
+        }
+    }
+
+    void AddMoney() {
+        money ++;
+        MoneyText.text = money.ToString();
+    }
+}
